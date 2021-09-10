b5689ed65017aba007b180732accec3049afab2e
Pablo P Varela <pablo@pablopunk.com>
2021-09-10 21:57:14 +0200

    Copyright

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -1,11 +1,10 @@
 import { FunctionComponent } from 'react'
-import { SITE_NAME } from 'config'
 
 type Props = {}
 
 const Footer: FunctionComponent<Props> = () => (
   <footer className="flex items-center justify-center h-footer bg-bgDim">
-    © {SITE_NAME} {new Date().getFullYear()}
+    © Pablo Varela {new Date().getFullYear()}
   </footer>
 )
 
