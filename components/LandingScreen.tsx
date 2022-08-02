6de1a4917edb187fd42f6cc7374817e2be4f4c56
Pablo P Varela <pablo@pablopunk.com>
2022-08-02 19:08:21 +0200

    add null screenshoot to prevent component shifting

--- components/LandingScreen.tsx
+++ components/LandingScreen.tsx
@@ -2,19 +2,23 @@ import Button from 'components/Button'
 import useTheme from 'hooks/useTheme'
 import { BsArrowRight } from 'react-icons/bs'
 
-export const LoadingScreen = () => {
+export const LandingScreen = () => {
   const [theme] = useTheme()
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-[max-content]">
+    <div className="w-full">
+      <div className="mx-auto w-[max-content]">
         <Button href="/login" type="action" className="text-xl nowrap">
           <span className="mr-1">Get started</span>
           <BsArrowRight />
         </Button>
       </div>
-      <div className="mt-2">
-        {theme && <img src={`/landing/${theme}.png`} alt="screenshot" />}
+      <div className="block mx-auto">
+        <img
+          src={`/landing/${theme}.png`}
+          alt="screenshot"
+          className="mx-auto"
+        />
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { LoadingScreen } from 'components/LandingScreen'
+import { LandingScreen } from 'components/LandingScreen'
 import UrlRedirects from 'components/UrlRedirects'
 import { useAuth } from 'db'
 import { FunctionComponent } from 'react'
@@ -8,7 +8,7 @@ type Props = {}
 const Index: FunctionComponent<Props> = () => {
   const { user } = useAuth()
 
-  return <>{user ? <UrlRedirects user={user} /> : <LoadingScreen />}</>
+  return <>{user ? <UrlRedirects user={user} /> : <LandingScreen />}</>
 }
 
 export default Index
