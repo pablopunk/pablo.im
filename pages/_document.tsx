15b3912563dc401830ba6ecbc3cae7741378acb2
Pablo P Varela <pablo@pablopunk.com>
2023-03-24 17:12:54 +0100

    feat: add lwt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,28 @@
 import Document, { Main, NextScript, Html, Head } from 'next/document'
 import darkModeCode from 'dark-mode-code'
 
+const lwtScript = `
+  (function (m, a, z, e) {
+    var s, t;
+    try {
+      t = m.sessionStorage.getItem('maze-us');
+    } catch (err) {}
+
+    if (!t) {
+      t = new Date().getTime();
+      try {
+        m.sessionStorage.setItem('maze-us', t);
+      } catch (err) {}
+    }
+
+    s = a.createElement('script');
+    s.src = z + '?t=' + t + '&apiKey=' + e;
+    s.async = true;
+    a.getElementsByTagName('head')[0].appendChild(s);
+    m.mazeUniversalSnippetApiKey = e;
+  })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '897eab31-050d-4b0c-8bc7-043ad0447d19');
+`
+
 export default class MyDocument extends Document {
   render() {
     return (
@@ -11,6 +33,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap"
             rel="stylesheet"
           />
+          <script dangerouslySetInnerHTML={{ __html: lwtScript }} />
         </Head>
         <body>
           <Main />
